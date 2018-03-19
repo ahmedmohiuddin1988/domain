ce1a8343d7e6503b0c881abab88a71a717222d83
ahmedmohiuddin1988 <ahmed_tariq240@hotmail.com>
2018-03-19 21:12:17 +1100

    Implementation of CRE Agency Business logic CRE Test Case Passed

--- Domain/Domain.Core/Business/Agency/CREAgency.cs
+++ Domain/Domain.Core/Business/Agency/CREAgency.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 using Domain.Core.Entity;
+using System.Text.RegularExpressions;
 
 namespace Domain.Core.Business.Agency
 {
     public class CREAgency : PropertyMatcher
     {
+        Regex regex = new Regex(@"\W+", RegexOptions.Compiled);
         public override string AgencyCode
         {
             get
@@ -17,8 +20,13 @@ namespace Domain.Core.Business.Agency
 
         public override bool IsMatch(Property agencyProperty, Property databaseProperty)
         {
-            // logic goes here
-            return false;
+            IEnumerable<string> agencyNameWords = regex.Split(agencyProperty.Name).Reverse();
+            var agencyName = string.Join(" ", agencyNameWords);
+
+            var databaseNameWords = regex.Split(databaseProperty.Name);
+            var databaseAgencyName = string.Join(" ", databaseNameWords);
+
+            return agencyName.Equals(databaseAgencyName, StringComparison.OrdinalIgnoreCase);
         }
     }
 }
